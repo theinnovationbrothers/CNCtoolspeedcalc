45254b7f788625318a1317fe4071c971ddc2a3b5
theinnovationbrothers <121250245+theinnovationbrothers@users.noreply.github.com>
2022-12-22 18:52:28 +0000

    Update toolspeedcalc.py

--- toolspeedcalc.py
+++ toolspeedcalc.py
@@ -1,29 +1,13 @@
-from flask import Flask, render_template, request
-import math
-
-app = Flask(__name__)
-
-@app.route("/")
-def calculator():
-    return render_template("toolspeedcalc.html")
-
-@app.route("/", methods=["POST"])
-def calculate():
-    # Get the input values from the form
-    fz = float(request.form["fz"])
-    fn = float(request.form["fn"])
-    dc = float(request.form["dc"])
-    z = float(request.form["z"])
-    Vc = float(request.form["Vc"])
-
-    # Calculate n
-    n = Vc * 1000 / (math.pi * dc)
-
-    # Calculate Vf
-    if fn != 0:
-        Vf = fn * n
-    else:
-        Vf = fz * z * n
-
-    # Render the calculator template with the calculated values
-    return render_template("toolspeedcalc.html", n=n, Vf=Vf)
+<form action="calculator.py" method="POST">
+  <label for="fz">fz:</label><br>
+  <input type="text" id="fz" name="fz"><br>
+  <label for="fn">fn:</label><br>
+  <input type="text" id="fn" name="fn"><br>
+  <label for="dc">dc:</label><br>
+  <input type="text" id="dc" name="dc"><br>
+  <label for="z">z:</label><br>
+  <input type="text" id="z" name="z"><br>
+  <label for="Vc">Vc:</label><br>
+  <input type="text" id="Vc" name="Vc"><br><br>
+  <button type="submit">Calculate</button>
+</form> 
